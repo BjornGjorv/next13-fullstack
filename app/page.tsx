8b68ce46e2245be12c917b5d072d6a55966d7c66
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-03-07 10:43:03 +0100

    Install and test tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <main>
-            <h1>Hello!</h1>
+        <main className="py-4 px-48">
+            <Link className="bg-teal-700 font-medium py-2 px-4 rounded-sm" href={"/dashboard"}>
+                Go to dashboard
+            </Link>
         </main>
     );
 }
