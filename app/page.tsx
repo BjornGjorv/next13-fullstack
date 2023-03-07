8cbeb9b6992388958b2846c51b55018c9d84d1bc
Bjørn Olav Gjørven <bjorn.gjorven@iterate.no>
2023-03-07 10:49:44 +0100

    Install typescript for node

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
     return (
         <main className="py-4 px-48">
-            <Link className="bg-teal-700 font-medium py-2 px-4 rounded-sm" href={"/dashboard"}>
+            <Link className="bg-teal-700 font-normal py-2 px-4 rounded-sm" href={"/dashboard"}>
                 Go to dashboard
             </Link>
         </main>
